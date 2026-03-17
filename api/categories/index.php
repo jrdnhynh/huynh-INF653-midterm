91f5b3c46f4485f4e42199c0a78d266ea429ffef
jrdnhynh <jrdnhynh@gmail.com>
2026-03-17 02:44:41 -0500

    Had a CORS error when trying to test so adding these files

---
+++ api/categories/index.php
@@ -0,0 +1,35 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// handle Pre-flight
+if ($method === 'OPTIONS') {
+    die();
+}
+
+// routing logic
+switch($method) {
+    case 'GET':
+        if (isset($_GET['id'])) {
+            require 'read_single.php';
+        } else {
+            require 'read.php';
+        }
+        break;
+    case 'POST':
+        require 'create.php';
+        break;
+    case 'PUT':
+        require 'update.php';
+        break;
+    case 'DELETE':
+        require 'delete.php';
+        break;
+    default:
+        echo json_encode(array('message' => 'Method Not Allowed'));
+        break;
+}
---
+++ api/quotes/index.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'OPTIONS') {
+    die();
+}
+
+// route to the correct file based on the method
+switch($method) {
+    case 'GET':
+        if (isset($_GET['id'])) {
+            require 'read_single.php';
+        } else {
+            require 'read.php';
+        }
+        break;
+    case 'POST':
+        require 'create.php';
+        break;
+    case 'PUT':
+        require 'update.php';
+        break;
+    case 'DELETE':
+        require 'delete.php';
+        break;
+}
